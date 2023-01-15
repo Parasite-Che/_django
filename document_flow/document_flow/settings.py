7e6bb89a0839ae76d9c09bcef96f36d9bbb899d1
Parasite-Che <tyurinip@yandex.ru>
2023-01-15 12:23:17 +0700

    15.01.2023
    starting work

--- document_flow/document_flow/settings.py
+++ document_flow/document_flow/settings.py
@@ -33,7 +33,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "polls.apps.PollsConfig",
-    "start.apps.StartConfig",
+    'users.apps.UsersConfig',
+    'crispy_forms',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -41,6 +42,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -129,3 +132,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_URL = 'login'
--- document_flow/polls/views.py
+++ document_flow/polls/views.py
@@ -1,33 +1,22 @@
 from django.shortcuts import render
 
-posts = [
-	{
-    	'author': 'Администратор',
-    	'title': 'Это первый пост',
-    	'content': 'Содержание первого поста.',
-    	'date_posted': '12 мая, 2022'
-	},
-	{
-    	'author': 'Пользователь',
-    	'title': 'Это второй пост',
-    	'content': 'Подробное содержание второго поста.',
-    	'date_posted': '13 мая, 2022'
-	}
-]
- 
+
 def base(request):
-	return render(request ,'polls/base.html' ,{
+    return render(request, 'polls/base.html', {
         'foo': 'bar',
     })
+
 
 def home(request):
-	return render(request ,'polls/home.html' ,{
+    return render(request, 'polls/home.html', {
         'foo': 'bar',
     })
 
+
 def about(request):
-	return render(request ,'polls/about.html' ,{
+    return render(request, 'polls/about.html', {
         'foo': 'bar',
     })
+
 
 # Create your views here.
